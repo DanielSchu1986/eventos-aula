6fa4b9df378972d41656fb711dbcd7671ab8e4fc
Bernardo Prates <bprates.ti@gmail.com>
2023-11-03 20:38:05 -0300

    03/11/2023 - 20:37

--- src/main/kotlin/br/upf/sistemadeeventos/repository/UsuarioRepository.kt
+++ src/main/kotlin/br/upf/sistemadeeventos/repository/UsuarioRepository.kt
@@ -4,11 +4,12 @@ import br.upf.sistemadeeventos.dtos.EventoRequestDTO
 import br.upf.sistemadeeventos.exceptions.NotFoundException
 import br.upf.sistemadeeventos.model.Evento
 import br.upf.sistemadeeventos.model.StatusEvento
+import br.upf.sistemadeeventos.model.Usuario
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.time.LocalDate
 
 @Repository
-interface EventoRepository: JpaRepository<Evento, Long> {
+interface UsuarioRepository: JpaRepository<Usuario, Long> {
 
 }
